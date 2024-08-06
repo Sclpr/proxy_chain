c36f950fecd5009424b958faf5c248835a4c649a
Sclpr <antntrdr@gmail.com>
2024-08-06 18:25:32 +0400

    Заменен curl на wget

--- ss.sh
+++ ss.sh
@@ -203,7 +203,7 @@ fi
 
 echo "Shadowsocks успешно установлен и запущен."
 echo "Конфигурация:"
-echo "Server IP: $(curl -s ifconfig.me)"
+echo "Server IP: $(wget -qO- ifconfig.me)"
 echo "Server Port: $SERVER_PORT"
 echo "Password: $PASSWORD"
 echo "Encryption Method: $METHOD"
